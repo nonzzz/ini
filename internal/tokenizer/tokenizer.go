8b6822ff0126e61b31aaf671da69d88a53f5bbec
kanno <812137533@qq.com>
2023-03-21 18:06:07 +0800

    refactor: lexer part

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -1,35 +1,12 @@
 package tokenizer
 
-import "fmt"
+type T uint8
 
 const (
-	TSection = "Section"
-	TIdent   = "Ident"
-	TComment = "Comment"
-	TAssign  = "Assign"
-	TKey     = "Key"
-	TValue   = "Value"
-	TEof     = "Eof"
+	TEof T = iota
+	TSection
+	TComment
+	TAssign
+	TKey
+	TValue
 )
-
-type Tokenizer struct {
-	Kind  string
-	Value string
-	Line  int
-}
-
-func NewToken(kind string, value string, line int) Tokenizer {
-
-	return Tokenizer{
-		Kind:  kind,
-		Value: value,
-		Line:  line,
-	}
-}
-
-func (token Tokenizer) String() string {
-	return fmt.Sprintf("{Kind:%s,Value:\"%s\" Linie:%d}",
-		token.Kind,
-		token.Value,
-		token.Line)
-}
